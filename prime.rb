8dc684f4d1f6d47dfbd81d28bff32a46d3c8db5f
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-02 01:00:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,16 @@
-def prime?
+def prime?(int)
   n = 0
   array = []
   while n < 100
      array << n
      n += 1
   end
-  array
+  m = 0
+  while m < 100
+    if int % array[m] == 0 
+      return FALSE
+    end
+  end
+  return TRUE
 end
+
